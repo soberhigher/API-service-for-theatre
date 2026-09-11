7d2e477c084f5492e57ecc05857412f95d4bb2e6
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-11 13:46:05 +0300

    create models Play with MtM Actor and Genre

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Play(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    actors = models.ManyToManyField("Actor", related_name="plays")
+    genres = models.ManyToManyField("Genre", related_name="plays")
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
