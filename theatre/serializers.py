daadb40e5ff24b04993f232f4c6111cb0c392edb
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-11 21:41:33 +0300

    Add PlayReadSerializer, PlayWriteSerializer instead PlaySerializers

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 
-from theatre.models import Play
+from theatre.models import Play, Actor, Genre
 
 
-class PlaySerializer(serializers.ModelSerializer):
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class PlayReadSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many=True, read_only=True)
+    genres = GenreSerializer(many=True, read_only=True)
+
     class Meta:
         model = Play
-        fields = "__all__"
+        fields = ["id", "title", "description", "actors", "genres"]
+
+class PlayWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Play
+        fields = ["id", "title", "description", "actors", "genres"]
