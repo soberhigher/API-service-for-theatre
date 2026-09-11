9e2a6bba432d877ecb889a43eeacb8486ea04e1f
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-11 21:49:47 +0300

    Add get_serializers_class for PlayViewSet

--- theatre/views.py
+++ theatre/views.py
@@ -2,11 +2,15 @@ from rest_framework import viewsets
 
 from theatre.models import Play
 from theatre.permissions import IsAdminOrReadOnly
-from theatre.serializers import PlaySerializer
+from theatre.serializers import PlayReadSerializer, PlayWriteSerializer
 
 
 class PlayViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
 
     queryset = Play.objects.all()
-    serializer_class = PlaySerializer
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return PlayReadSerializer
+        return PlayWriteSerializer
