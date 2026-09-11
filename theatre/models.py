e6d93eac8807b8d8931233aa0e41330eeb22409d
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-11 14:00:51 +0300

    create TheatreHall and Reservation models

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -33,3 +35,26 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Reservation {self.created_at} from {self.user}"
