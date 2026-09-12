6cab25935b0c390a9b9d9d79a9db399ac34ad9df
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-12 10:12:43 +0300

    Add urls for actors, genres

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from theatre.views import PlayViewSet
+from theatre.views import (PlayViewSet,
+                           ActorViewSet,
+                           GenreViewSet
+                           )
 
 
 router = DefaultRouter()
 
 router.register(r"plays", PlayViewSet)
+router.register(r"actors", ActorViewSet)
+router.register(r"genres", GenreViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
