4c05a6a44b56085d70c0e7653e79a2446ddb0ecc
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-13 19:06:56 +0300

    Add TicketSerializer and def create logic for ReservationSerializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.password_validation import validate_password
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
@@ -6,7 +7,9 @@ from theatre.models import (Play,
                             Actor,
                             Genre,
                             TheatreHall,
-                            Performance, Reservation
+                            Performance,
+                            Reservation,
+                            Ticket
                             )
 
 from django.contrib.auth.models import User
@@ -105,8 +108,74 @@ class PerformanceWriteSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = "__all__"
+        read_only_fields = ["id", "reservation"]
+
+
 class ReservationSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True)
+
     class Meta:
         model = Reservation
-        fields = "__all__"
+        fields = ["id", "created_at", "user", "tickets"]
         read_only_fields = ["id", "created_at", "user"]
+
+    def validate_tickets(self, tickets):
+        if not tickets:
+            raise serializers.ValidationError(
+                "Reservation must contain at least one ticket"
+            )
+        selected_places = set()
+
+        for ticket in tickets:
+            performance = ticket["performance"]
+            row = ticket["row"]
+            seat = ticket["seat"]
+            hall = performance.theatre_hall
+
+            place = (performance.id, row, seat)
+
+            if Ticket.objects.filter(
+                performance=performance,
+                row=row,
+                seat=seat
+            ).exists():
+                raise serializers.ValidationError(
+                    "This seat is already reserved"
+                )
+
+            if place in selected_places:
+                raise serializers.ValidationError(
+                    "The same seat cannot be selected more than once."
+                )
+            selected_places.add(place)
+
+            if row < 1 or row > hall.rows:
+                raise serializers.ValidationError(
+                    f"Row must be between 1 and {hall.rows}"
+                )
+            if seat < 1 or seat > hall.seats_in_row:
+                raise serializers.ValidationError(
+                    f"Seat must be between 1 and {hall.seats_in_row}."
+                )
+
+        return tickets
+
+
+    @transaction.atomic
+    def create(self, validated_data):
+        tickets_data = validated_data.pop("tickets")
+
+        reservation = Reservation.objects.create(
+            **validated_data,
+        )
+
+        for ticket_data in tickets_data:
+            Ticket.objects.create(
+                reservation=reservation,
+                **ticket_data,
+            )
+        return reservation
