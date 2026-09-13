c85d9a7765684fd64e173d22d3b7fb246ca1e9a1
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-13 21:19:02 +0300

    Add TicketViewSet

--- theatre/views.py
+++ theatre/views.py
@@ -10,7 +10,7 @@ from theatre.models import (Play,
                             Genre,
                             TheatreHall,
                             Performance,
-                            Reservation
+                            Reservation, Ticket
                             )
 from theatre.permissions import IsAdminOrReadOnly
 from theatre.serializers import (PlayReadSerializer,
@@ -21,7 +21,7 @@ from theatre.serializers import (PlayReadSerializer,
                                  PerformanceReadSerializer,
                                  PerformanceWriteSerializer,
                                  RegistrationSerializer,
-                                 ReservationSerializer
+                                 ReservationSerializer, TicketSerializer, TicketPurchaseSerializer
                                  )
 
 
@@ -97,3 +97,34 @@ class ReservationViewSet(viewsets.ModelViewSet):
 
     def partial_update(self, request, *args, **kwargs):
         raise MethodNotAllowed("PATCH")
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Ticket.objects.all()
+
+    def get_queryset(self):
+        return Ticket.objects.filter(reservation__user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return TicketPurchaseSerializer
+        return TicketSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        current_time = timezone.now()
+
+        if instance.performance.show_time <= current_time:
+            raise serializers.ValidationError(
+                "Ticket cannot be cancelled after the performance has started"
+            )
+        instance.delete()
+
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed("PUT")
+
+    def partial_update(self, request, *args, **kwargs):
+        raise MethodNotAllowed
