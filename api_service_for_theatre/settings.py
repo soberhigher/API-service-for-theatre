1e0fd81c41f7b49a2430fbf141c018dfb29a0535
Danylo Pylypenko <p.prince.e777@gmail.com>
2026-09-14 18:56:14 +0300

    Add API request throttling

--- api_service_for_theatre/settings.py
+++ api_service_for_theatre/settings.py
@@ -135,6 +135,14 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/min",
+        "user": "30/min",
+    },
 }
 
 SIMPLE_JWT = {
